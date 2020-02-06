cc2b159d8c30c68696e962a0887a43cef0a608bc
FaLLeN <amitabhamajumder2@gmail.com>
2020-02-06 19:15:58 +0530

    changed admin_site header and admin _site title

--- room/room/urls.py
+++ room/room/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header='admin site'
+admin.site.site_title='admin login'
+
 urlpatterns = [
+    # for admin site 
     path('admin/', admin.site.urls),
 
     # searches for urls file in home app 
